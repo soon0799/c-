a2eb921de39bd04c3be5b081bcf08e523ced99b9
soon0799 <qkrtns123@naver.com>
2021-03-15 10:42:20 +0900

    Add files via upload

---
+++ 0315/PropertyApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class PropertyClass
+{
+	private int privateValue;
+	public int PrivateValue
+	{
+		get { return privateValue; }
+		set { privateValue = value; }
+	}
+
+	public void PrintValue()
+	{
+		Console.WriteLine("Hidden Value = " + privateValue);
+	}
+}
+namespace Property
+{
+    class PropertyApp
+    {
+        static void Main(string[] args)
+        {
+			int n;
+			PropertyClass obj = new PropertyClass();
+			obj.PrivateValue = 100;
+			obj.PrintValue();
+			n = obj.PrivateValue;
+			Console.WriteLine("Value = " + n);
+		}
+    }
+}
