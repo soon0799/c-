e19cd1386dfd21e7c2691990200f5945b67fd0bf
soon0799 <qkrtns123@naver.com>
2021-03-22 11:23:43 +0900

    Add files via upload

---
+++ 0322/01Console.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Basic
+{
+    class Console
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hello ");
+            System.Console.WriteLine("World!");
+            System.Console.Write("이름을 입력하세요: ");
+
+            string name = System.Console.ReadLine();
+            System.Console.Write("안녕하세요, ");
+            System.Console.Write(name);
+            System.Console.WriteLine("님!");
+
+            string name1 = System.Console.ReadLine();
+        }
+    }
+}
---
+++ 0322/16DevideByZero.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+namespace Basic
+{
+    class DivideByZero
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 0;
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
