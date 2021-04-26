012c8545d6010dc47b002b1f0bab1f6c851d84fd
soon0799 <qkrtns123@naver.com>
2021-04-26 21:53:59 +0900

    Add files via upload

---
+++ 0426/23DivByZeroExceptionApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DivByZeroExceptionApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int i = 1, j = 0, k;
+            k = i / j;
+        }
+    }
+}
---
+++ 0426/24IndexOutOfRangeApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IndexOutOfRangeApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] vector = { 1, 2, 3 };
+            vector[4] = 4;
+        }
+    }
+}
