0b6e5aaf3bc9aa9d0c4dc46896088f139d7e4b4e
soon0799 <qkrtns123@naver.com>
2021-05-17 12:15:23 +0900

    Add files via upload

---
+++ databinding/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DataBinding
+{
+
+    public partial class Form2 : Form
+    {
+        static public string text;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Form1 f = new Form1();
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
